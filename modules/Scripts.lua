bc0ead6fddb3bcdf7822389704e9513c4ab3cead
DOT-DHA <futurespacepresident@gmail.com>
2022-06-12 12:46:37 -0500

    1.0

--- modules/Scripts.lua
+++ modules/Scripts.lua
@@ -4,7 +4,18 @@ function Command_Rainbow(args)
 
   -- items, objects, and menu objects use your MOD_NAME in their oid
   api_give_item(MOD_NAME .. "_hair_dye_red", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_red", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_orange", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_yellow", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_green", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_teal", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_blue", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_royal_blue", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_purple", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_pink", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_remover", 10)
 
   -- flowers, bees, walls and NPCs DO NOT use your MOD_NAME in their oid!
-
+  
+  api_give_item("npc51", 1)
 end
--- modules/define.lua
+++ modules/define.lua
@@ -5,12 +5,93 @@ function define_items()
   -- define custom items
   api_define_item({
     id = "hair_dye_red",
-    name = "Red Box Dye",
+    name = "Red Dye",
     category = "Decoration",
     tooltip = "Right click to dye your hair.",
     shop_buy = 15,
     shop_sell = 5
-  }, "sprites/item/axe_item.png")
+  }, "sprites/item/hair_dye_red.png")
+
+  api_define_item({
+    id = "hair_dye_orange",
+    name = "Orange Dye",
+    category = "Decoration",
+    tooltip = "Right click to dye your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_orange.png")
+
+  api_define_item({
+    id = "hair_dye_yellow",
+    name = "Yellow Dye",
+    category = "Decoration",
+    tooltip = "Right click to dye your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_yellow.png")
+  
+  api_define_item({
+    id = "hair_dye_green",
+    name = "Green Dye",
+    category = "Decoration",
+    tooltip = "Right click to dye your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_green.png")
+
+  api_define_item({
+    id = "hair_dye_teal",
+    name = "Teal Dye",
+    category = "Decoration",
+    tooltip = "Right click to dye your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_teal.png")
+
+  api_define_item({
+    id = "hair_dye_blue",
+    name = "Blue Dye",
+    category = "Decoration",
+    tooltip = "Right click to dye your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_blue.png")
+
+  api_define_item({
+    id = "hair_dye_royal_blue",
+    name = "Royal Blue Dye",
+    category = "Decoration",
+    tooltip = "Right click to dye your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_royal_blue.png")
+
+  api_define_item({
+    id = "hair_dye_purple",
+    name = "Purple Dye",
+    category = "Decoration",
+    tooltip = "Right click to dye your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_purple.png")
+
+  api_define_item({
+    id = "hair_dye_pink",
+    name = "Pink Dye",
+    category = "Decoration",
+    tooltip = "Right click to dye your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_pink.png")
+
+  api_define_item({
+    id = "hair_dye_remover",
+    name = "Dye Remover",
+    category = "Decoration",
+    tooltip = "Right click to remove dye from your hair.",
+    shop_buy = 15,
+    shop_sell = 5
+  }, "sprites/item/hair_dye_remover.png")
 
   -- add the item to the workbench as a recipe
   recipe = {
@@ -18,6 +99,63 @@ function define_items()
     { item = "sawdust1", amount = 5 }
   }
   res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_red", recipe)
+
+  recipe = {
+    { item = "dye5", amount = 10 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_orange", recipe)
+
+  recipe = {
+    { item = "dye3", amount = 10 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_yellow", recipe)
+
+  recipe = {
+    { item = "dye4", amount = 10 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_green", recipe)
+
+  recipe = {
+    { item = "dye2", amount = 7 },
+    { item = "dye8", amount = 3 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_teal", recipe)
+
+  recipe = {
+    { item = "dye2", amount = 10 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_blue", recipe)
+
+  recipe = {
+    { item = "dye2", amount = 7 },
+    { item = "dye7", amount = 3 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_royal_blue", recipe)
+
+  recipe = {
+    { item = "dye6", amount = 10 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_purple", recipe)
+
+  recipe = {
+    { item = "dye1", amount = 7 },
+    { item = "dye8", amount = 3 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_pink", recipe)
+
+  recipe = {
+    { item = "dye9", amount = 10 },
+    { item = "sawdust1", amount = 5 }
+  }
+  res = api_define_recipe("painting", MOD_NAME .. "_hair_dye_remover", recipe)
 
 end
 
@@ -30,28 +168,39 @@ function define_npc()
     name = "Cathy",
     pronouns = "She/Her",
     tooltip = "Need a new look?",
-    shop = true,
-    walking = true,
-    stock = {MOD_NAME .. "_hair_dye_red"}, -- max 10
-    specials = {"log", "log", "log"}, -- must be 3
+    specials = {}, -- must be 3
+    stock = {
+        MOD_NAME .. "_hair_dye_red",
+        MOD_NAME .. "_hair_dye_orange",
+        MOD_NAME .. "_hair_dye_yellow",
+        MOD_NAME .. "_hair_dye_green",
+        MOD_NAME .. "_hair_dye_teal",
+        MOD_NAME .. "_hair_dye_blue",
+        MOD_NAME .. "_hair_dye_royal_blue",
+        MOD_NAME .. "_hair_dye_purple",
+        MOD_NAME .. "_hair_dye_pink",
+        MOD_NAME .. "_hair_dye_remover"
+    }, -- max 10
+    greeting = "Greeting",
     dialogue = {
-      "Wot ya mean av I gots anything other than logs to sell??",
-      "Wot a stoopid question hoomie"
+      "Dialogue 1",
+      "Dialogue 2"
     },
-    greeting = "Alright pal ow ya gettin' on"
+    walking = true,
+    shop = true
   }
 
   -- define npc
   api_define_npc(npc_def,
-    "sprites/npc_standing.png",
-    "sprites/npc_standing_h.png",
-    "sprites/npc_walking.png",
-    "sprites/npc_walking_h.png",
-    "sprites/npc_head.png",
-    "sprites/npc_bust.png",
-    "sprites/npc_item.png",
-    "sprites/npc_dialogue_menu.png",
-    "sprites/npc_shop_menu.png"
+    "sprites/npc/npc_standing.png",
+    "sprites/npc/npc_standing_h.png",
+    "sprites/npc/npc_walking.png",
+    "sprites/npc/npc_walking_h.png",
+    "sprites/npc/npc_head.png",
+    "sprites/npc/npc_bust.png",
+    "sprites/npc/npc_item.png",
+    "sprites/npc/npc_dialogue_menu.png",
+    "sprites/npc/npc_shop_menu.png"
   )
 
 end
