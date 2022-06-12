13540530f0b0b11e51fc4e5742d3f40ef4e26517
DOT-DHA <futurespacepresident@gmail.com>
2022-06-12 17:00:19 -0500

    1.0

--- modules/Scripts.lua
+++ modules/Scripts.lua
@@ -14,6 +14,9 @@ function Command_Rainbow(args)
   api_give_item(MOD_NAME .. "_hair_dye_purple", 10)
   api_give_item(MOD_NAME .. "_hair_dye_pink", 10)
   api_give_item(MOD_NAME .. "_hair_dye_remover", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_mushy", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_zoobot", 10)
+  api_give_item(MOD_NAME .. "_hair_dye_beenus", 10)
 
   -- flowers, bees, walls and NPCs DO NOT use your MOD_NAME in their oid!
   
