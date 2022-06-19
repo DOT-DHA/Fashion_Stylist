4b31dde07db8f7130453477c0d3bad403f81c848
DOT-DHA <futurespacepresident@gmail.com>
2022-06-18 22:10:14 -0500

    2.0

--- mod.lua
+++ mod.lua
@@ -5,25 +5,25 @@ MOD_NAME = "fashion_stylist"
 
 --first is light then base then dark
 local base_colors = {
-    {  {68, 100, 131},    {50, 74, 98},    {36, 55, 82} }, 
-    {  {118, 91, 105},    {94, 73, 90},    {67, 55, 74} }, 
-    { {149, 117, 126},  {118, 91, 105},    {94, 73, 90} }, 
-    { {162, 134, 142}, {149, 117, 126},  {118, 91, 105} }, 
-    {   {115, 60, 78},    {97, 50, 66},    {79, 41, 56} }, 
-    {   {174, 94, 94},   {145, 71, 78},   {122, 53, 68} }, 
-    {  {212, 134, 83},  {202, 118, 78},   {161, 80, 63} }, 
-    {  {237, 184, 92},  {209, 147, 71},  {191, 120, 65} }, 
-    { {250, 211, 112},  {237, 184, 92},  {209, 147, 71} }, 
-    {  {93, 176, 154},  {75, 155, 142},  {55, 110, 114} }, 
-    {  {87, 142, 154},  {66, 111, 128},   {51, 90, 112} }, 
-    {  {69, 165, 201},  {59, 142, 186},  {46, 113, 174} }, 
-    { {113, 186, 214},  {69, 165, 201},  {59, 142, 186} }, 
-    { {200, 213, 218}, {166, 184, 196}, {147, 166, 183} }, 
-    { {233, 230, 226}, {207, 204, 202}, {168, 166, 165} }, 
-    { {247, 219, 203}, {235, 204, 186}, {227, 185, 161} }, 
-    { {235, 204, 186}, {227, 185, 161}, {217, 163, 139} }, 
-    { {227, 185, 161}, {217, 163, 139}, {194, 121, 103} }, 
-    { {217, 163, 139}, {194, 131, 116},  {176, 109, 99} }, 
+    {  {68, 100, 131},    {50, 74, 98},    {36, 55, 82} },
+    {  {118, 91, 105},    {94, 73, 90},    {67, 55, 74} },
+    { {149, 117, 126},  {118, 91, 105},    {94, 73, 90} },
+    { {162, 134, 142}, {149, 117, 126},  {118, 91, 105} },
+    {   {115, 60, 78},    {97, 50, 66},    {79, 41, 56} },
+    {   {174, 94, 94},   {145, 71, 78},   {122, 53, 68} },
+    {  {212, 134, 83},  {202, 118, 78},   {161, 80, 63} },
+    {  {237, 184, 92},  {209, 147, 71},  {191, 120, 65} },
+    { {250, 211, 112},  {237, 184, 92},  {209, 147, 71} },
+    {  {93, 176, 154},  {75, 155, 142},  {55, 110, 114} },
+    {  {87, 142, 154},  {66, 111, 128},   {51, 90, 112} },
+    {  {69, 165, 201},  {59, 142, 186},  {46, 113, 174} },
+    { {113, 186, 214},  {69, 165, 201},  {59, 142, 186} },
+    { {200, 213, 218}, {166, 184, 196}, {147, 166, 183} },
+    { {233, 230, 226}, {207, 204, 202}, {168, 166, 165} },
+    { {247, 219, 203}, {235, 204, 186}, {227, 185, 161} },
+    { {235, 204, 186}, {227, 185, 161}, {217, 163, 139} },
+    { {227, 185, 161}, {217, 163, 139}, {194, 121, 103} },
+    { {217, 163, 139}, {194, 131, 116},  {176, 109, 99} },
     {  {149, 108, 97},   {129, 88, 84},   {112, 73, 74} } }
 
 function register()
@@ -38,37 +38,36 @@ end
 function init()
     --Turn on DevMode(logs T/F, Devmode T/F)
     --Devmode function in scripts.lua
-    
+
     DevMode(true, true)
 
     devlog("init", "start")
 
     define_hair_items()
-    
+
     define_hair_npc()
 
     define_overall_items()
-    
+
     define_overall_npc()
 
     devlog("init", "end")
-    
 
     return "Success"
 end
 
 function ready()
-    
+
     local player = api_get_player_instance()
-    api_dp(player, "Hair-Dye", "none")
+    api_define_property(player, "Hair-Dye", "none")
 
     local npcs = {
         {api_get_menu_objects(nil, "npc51"), "npc51"},
         {api_get_menu_objects(nil, "npc52"), "npc52"} }
-    
-    
+
+
     for Key, Value in pairs(npcs) do
-    
+
         if #Value[1] == 0 then
             local playerPos = api_get_player_position()
             api_create_obj(Value[2], playerPos["x"] + 16, playerPos["y"] - 32)
@@ -91,19 +90,19 @@ function clock()
     local npc = api_get_menu_objects(nil, "npc51")
     local shopOpen
 
-    if npc then shopOpen = api_gp(api_gp(npc[1]["menu_id"], "shop"), "open") end
+    if npc then shopOpen = api_get_property(api_get_property(npc[1]["menu_id"], "shop"), "open") end
 
     if shopOpen then
         rotate_stock("npc51", {})
     end
-    
+
     devlog("clock", tick and "tick" or "tock")
 end
 
 function rotate_stock(npc_id, stock_table)
     local npc_object = api_get_menu_objects(nil, npc_id)
-    if npc_object then local shop_id = api_gp(npc_object[1]["menu_id"], "shop") end
-    
+    if npc_object then local shop_id = api_get_property(npc_object[1]["menu_id"], "shop") end
+
 --[[
 
     if hairIndex > #Hair_Dye then hairIndex = 1 end
@@ -121,7 +120,7 @@ function rotate_stock(npc_id, stock_table)
           api_slot_clear(shop_slots[i+1]["id"])
         end
       end
-    
+
       if stock_table[1]+1 == #stock_table then
         stock_table[1] = 1
       else
@@ -137,8 +136,8 @@ function click(button, click_type)
 
     local player = api_get_player_instance()
     local mouse = api_get_mouse_inst()
-    local palette = api_gp(player, "pal")
-    local basePalette = api_gp(player,"palette")
+    local palette = api_get_property(player, "pal")
+    local basePalette = api_get_property(player,"palette")
     local pSlots = api_get_slots(player)
     local done = false
 
@@ -150,13 +149,13 @@ function click(button, click_type)
 
             if mouse["item"] == MOD_NAME.."_hair_dye_remover" then
 
-                if api_gp(player, "Hair-Dye") == "none" then 
+                if api_get_property(player, "Hair-Dye") == "none" then
 
                     --Create a notification
                     api_set_notification("notice", "fashion_stylist_hair_dye_remover", "Dye Removed", "Cant remove any more dye")
                     done = true
 
-                elseif api_gp(player, "Hair-Dye") ~= "none" then 
+                elseif api_get_property(player, "Hair-Dye") ~= "none" then
 
                     --Prevent halving a stack
                     if mouse["count"] > 0 then
@@ -184,13 +183,13 @@ function click(button, click_type)
 
                     if mouse["item"] == Value[1] then
 
-                        if api_gp(player, "Hair-Dye") == Value[1] then 
-                        
+                        if api_get_property(player, "Hair-Dye") == Value[1] then
+
                             --Create a notification
                             api_set_notification("notice", Value[2], "Already Dyed", "Cant dye any more")
                             done = true
 
-                        else 
+                        else
 
                             --Prevent halving a stack
                             if mouse["count"] > 0 then
@@ -221,13 +220,13 @@ function click(button, click_type)
 
                     if mouse["item"] == Value[2] then
 
-                        if api_gp(player, "Hair-Dye") == Value[1] then 
-                        
+                        if api_get_property(player, "Hair-Dye") == Value[1] then
+
                             --Create a notification
                             api_set_notification("notice", Value[2], "Already Dyed", "Cant dye any more")
 
                         else
-                        
+
                             --Prevent halving a stack
                             if mouse["count"] > 0 then
                                 api_sp(slot["id"], "count", slot["count"] + mouse["count"])
